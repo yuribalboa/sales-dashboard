dadcf4202b8f4a66e548957c51a84c7737929a65
YuriTropeia <yurineudinetropeia@gmail.com>
2024-10-19 08:20:37 -0300

    Material-UI-Insert

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,20 +1,29 @@
-import styled from 'styled-components'
+import { Box, Container, Grid } from '@mui/material'
+import { BannerImage } from '@/components'
 
-const RegistrationArea = styled.div`
-  background: #333;
-`
-
-const RegistrationImage = styled.div`
-  background-image: url(/login-image.svg);
-  background-size: cover;
-  height: 100vh;
-  width: 50vw;
-`
 function Registration() {
   return (
     <>
-      <RegistrationArea>Registration</RegistrationArea>
-      <RegistrationImage />
+      <Box>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            sx={{ alignItems: 'center', display: 'flex', height: '100vh' }}
+          >
+            <Container maxWidth="sm">
+              <h1>CADASTRO</h1>
+            </Container>
+          </Grid>
+
+          <Grid item sm={6} sx={{ display: { xs: 'none', sm: 'block' } }}>
+            <Container maxWidth="sm">
+              <BannerImage />
+            </Container>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
