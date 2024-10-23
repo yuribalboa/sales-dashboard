f97e4ae842bfa936aad143a2d7e72378191605d3
YuriTropeia <yurineudinetropeia@gmail.com>
2024-10-22 20:42:51 -0500

    header-component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as FormComponent } from './FormComponent'
+export { default as Header } from './Header'
 export * from './BannerImage'
 export * from './Logo'
 export * from './StyledButton'
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,9 @@
+import { Header } from '@/components'
+
 function Profile() {
   return (
     <>
+      <Header />
       <h1>Profile</h1>
     </>
   )
