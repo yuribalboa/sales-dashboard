9dbfc2dda58c572683ce6358a913d444bbef420d
YuriTropeia <yurineudinetropeia@gmail.com>
2024-10-23 10:59:20 -0500

    avatars-list-component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as AvatarList } from './AvatarList'
 export { default as FormComponent } from './FormComponent'
 export { default as Header } from './Header'
 export * from './BannerImage'
