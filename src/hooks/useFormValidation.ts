7a70e7ee1f104ba07a574c52a02539dc8123df92
YuriTropeia <yurineudinetropeia@gmail.com>
2024-10-29 15:31:59 -0500

    create profile flow (sem conexao com a API)

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -17,7 +17,18 @@ export const useFormValidation = (inputs: InputProps[]) => {
         return /\S+@\S+\.\S+/.test(String(formValues[index]))
       }
       if (input.type === 'password') {
-        return String(formValues[index]).length > 7
+        const password = String(value)
+        const hasCorrectLenght = password.length >= 8 && password.length <= 16
+        const hasUppercaseLetter = /[A-Z]/.test(password)
+        const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+        const hasNumber = /\d/.test(password)
+
+        return (
+          hasCorrectLenght &&
+          hasUppercaseLetter &&
+          hasSpecialCharacter &&
+          hasNumber
+        )
       }
       return true
     })
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { AppThemeProvider } from './contexts/AppThemeContext.tsx'
 import { GlobalStyle } from './styles/'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import store from './redux/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppThemeProvider>
-      <GlobalStyle />
-      <App />
-    </AppThemeProvider>
+    <Provider store={store}>
+      <AppThemeProvider>
+        <GlobalStyle />
+        <App />
+      </AppThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
