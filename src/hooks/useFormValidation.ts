c17370cd70f5dbb0132402f25a4cf38d86ec4f75
YuriTropeia <yurineudinetropeia@gmail.com>
2024-10-28 20:23:50 -0500

    profile requests (erros devido a API fora do ar)

--- src/hooks/useFormValidation.ts
+++ src/hooks/useFormValidation.ts
@@ -9,6 +9,10 @@ export const useFormValidation = (inputs: InputProps[]) => {
 
   useEffect(() => {
     const allFieldsValid = inputs.every((input, index) => {
+      const value = formValues[index]
+      if (input.required && !value) {
+        return false
+      }
       if (input.type === 'email') {
         return /\S+@\S+\.\S+/.test(String(formValues[index]))
       }
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,178 @@
-import { useContext } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ChangeEvent, useContext, useEffect, useState } from 'react'
 import { AppThemeContext } from '@/contexts/AppThemeContext'
+import Cookies from 'js-cookie'
 
 //COMPONENTS
-import { CardComponents, Header, StyledButton, StyledH2 } from '@/components'
+import {
+  CardComponents,
+  FormComponent,
+  Header,
+  StyledButton,
+  StyledH2,
+} from '@/components'
 import { /*Card,*/ Container, Grid } from '@mui/material'
+
+//HOOK
+import { useFormValidation, useGet, usePut, useDelete } from '@/hooks'
 
 //SERVICES
 import { logout } from '@/services'
 
+//TYPES
+import {
+  InputProps,
+  ProfileData,
+  ProfileEditableData,
+  MessageProps,
+} from '@/types'
+
 function Profile() {
   const themeContext = useContext(AppThemeContext)
+
+  //HOOKS
+  const [updatedMessage, setUpdatedMessage] = useState<MessageProps>({
+    type: 'success',
+    msg: '',
+  })
+
+  const clearMessage = () => {
+    setTimeout(() => {
+      setUpdatedMessage({
+        type: 'success',
+        msg: '',
+      })
+    }, 3000)
+  }
+
+  const {
+    data: profileData,
+    loading: profileLoading,
+    error: profileError,
+  } = useGet<ProfileData>('profile')
+
+  const {
+    data: profileUpdatedData,
+    putData: profilePutData,
+    loading: profileUpdatedLoading,
+    error: profileUpdatedError,
+  } = usePut<ProfileEditableData>('profile/update')
+
+  const { deleteData: profileDeleteData, loading: profileDeleteLoading } =
+    useDelete('profile/update')
+
+  useEffect(() => {
+    if (profileData) {
+      handleChange(0, profileData.name)
+      handleChange(1, profileData.email)
+      handleChange(2, profileData.phone)
+    }
+  }, [profileData])
+
+  //FORM
+  const inputs: InputProps[] = [
+    { name: 'name', type: 'text', placeholder: 'Nome', required: true },
+    { name: 'email', type: 'email', placeholder: 'Email', disabled: true },
+    { name: 'phone', type: 'tel', placeholder: 'Telefone', required: true },
+  ]
+  const { formValues, formValid, handleChange } = useFormValidation(inputs)
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    await profilePutData({
+      name: String(formValues[0]),
+      phone: String(formValues[2]),
+    })
+  }
+
+  const handleDelete = async () => {
+    if (
+      confirm(
+        'Tem certeza que deseja excluir sua conta? Se sim, certifique-se de deletar os seus leads antes.'
+      ) === true
+    ) {
+      try {
+        await profileDeleteData()
+        alert('Perfil deletado com sucesso!')
+        Cookies.remove('Authorization')
+        window.location.href = '/'
+      } catch (e) {
+        alert(
+          'Não foi possível realizar a operação. Entre em contato com nosso suporte.'
+        )
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (profileUpdatedData !== null) {
+      setUpdatedMessage({
+        msg: 'Perfil atualizado com sucesso.',
+        type: 'success',
+      })
+    } else if (profileUpdatedError) {
+      setUpdatedMessage({
+        msg: 'Não foi possível realizar a operação.',
+        type: 'error',
+      })
+    }
+    clearMessage()
+  }, [profileUpdatedData, profileUpdatedError])
+
   return (
     <>
       <Header />
       <Container className="mb-2" maxWidth="lg">
         <Grid container spacing={4}>
           <Grid item xs={12} sm={6}>
-            <CardComponents>Seus dados...</CardComponents>
+            {!profileError && (
+              <CardComponents
+                className={
+                  profileLoading ? 'skeleton-loading skeleton-loading-mh-2' : ''
+                }
+              >
+                {!profileLoading && profileData && (
+                  <>
+                    <StyledH2 className="mb-1">Seus Dados</StyledH2>
+                    <FormComponent
+                      inputs={inputs.map((input, index) => ({
+                        ...input,
+                        type: input.type,
+                        placeholder: input.placeholder,
+                        value: formValues[index] || '',
+                        onChange: (e: ChangeEvent<HTMLInputElement>) =>
+                          handleChange(
+                            index,
+                            (e.target as HTMLInputElement).value
+                          ),
+                      }))}
+                      buttons={[
+                        {
+                          className: 'primary',
+                          disabled: !formValid || profileUpdatedLoading,
+                          type: 'submit',
+                          onClick: handleSubmit,
+                          children: profileUpdatedLoading
+                            ? 'Aguarde...'
+                            : 'Atualizar meu perfil',
+                        },
+
+                        {
+                          className: 'alert',
+                          disabled: profileDeleteLoading,
+                          type: 'button',
+                          onClick: handleDelete,
+                          children: profileDeleteLoading
+                            ? 'Aguarde...'
+                            : 'Excluir minha conta',
+                        },
+                      ]}
+                      message={updatedMessage}
+                    />
+                  </>
+                )}
+              </CardComponents>
+            )}
           </Grid>
           <Grid item xs={12} sm={6}>
             <CardComponents>
